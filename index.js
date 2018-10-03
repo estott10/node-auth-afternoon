16738cf99399a6d12d6e39b3bc953662465294ec
ErikaStott <erika.r.stott@gmail.com>
2018-10-03 13:08:03 -0700

    completed

--- index.js
+++ index.js
@@ -1,48 +1,61 @@
-require('dotenv').config();
 
+require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
 const Auth0Strategy = require('passport-auth0');
+const students = require('./students.json');
 
 const app = express();
 
-app.use(session({
-    secret: "Happy happy joy joy",
-    resave: false,
-    saveUninitialized: false
+app.use( session({
+  secret: '@nyth!ng y0u w@nT',
+  resave: false,
+  saveUninitialized: false
 }));
-
 
 app.use( passport.initialize() );
 app.use( passport.session() );
-app.use( passport.Auth0Strategy({
-    domain: process.env.DOMAIN,
-    clientID: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: './login',
-    scope: "openid email profile"
-}) )
+passport.use( new Auth0Strategy({
+  domain:       process.env.DOMAIN,
+  clientID:     process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL:  '/login',
+  scope: "openid email profile"
+ },
+ function(accessToken, refreshToken, extraParams, profile, done) {
+   // accessToken is the token to call Auth0 API (not needed in the most cases)
+   // extraParams.id_token has the JSON Web Token
+   // profile has all the information from the user
+   return done(null, profile);
+ }
+) );
 
+passport.serializeUser( (user, done) => {
+  done(null, { clientID: user.id, email: user._json.email, name: user._json.name });
+});
 
-passport.serializeUser(function(user, done){
-    done({
-        clientID: user.id,
-        email: user._json.email,
-        name: user._json.name
-    })
-})
+passport.deserializeUser( (obj, done) => {
+  done(null, obj);
+});
 
-passport.deserializeUser(function(obj, done){
-    done(null, obj);
-})
+app.get( '/login',
+  passport.authenticate('auth0',
+    { successRedirect: '/students', failureRedirect: '/login', connection: 'github' }
+  )
+);
 
-app.get('/login', passport.authenticate('auth0', {
-    sucessRedirect: '/students',
-    failureRedirect: '/login',
-    connection: 'github'
+function authenticated(req, res, next) {
+    if( req.user ) {
+      next()
+    } else {
+      res.sendStatus(401);
     }
-));
+  }
+
+  app.get('/students', authenticated, ( req, res, next ) => {
+    res.status(200).send(students)
+  });
 
 const port = 3000;
 app.listen( port, () => { console.log(`Server listening on port ${port}`); } );

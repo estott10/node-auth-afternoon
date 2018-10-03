91d747474d8026361aff92ce28d58dc552355ddf
ErikaStott <erika.r.stott@gmail.com>
2018-10-02 20:21:49 -0700

    through step 6

--- index.js
+++ index.js
@@ -1,8 +1,48 @@
+require('dotenv').config();
+
 const express = require('express');
 const session = require('express-session');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
 
 const app = express();
 
+app.use(session({
+    secret: "Happy happy joy joy",
+    resave: false,
+    saveUninitialized: false
+}));
+
+
+app.use( passport.initialize() );
+app.use( passport.session() );
+app.use( passport.Auth0Strategy({
+    domain: process.env.DOMAIN,
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: './login',
+    scope: "openid email profile"
+}) )
+
+
+passport.serializeUser(function(user, done){
+    done({
+        clientID: user.id,
+        email: user._json.email,
+        name: user._json.name
+    })
+})
+
+passport.deserializeUser(function(obj, done){
+    done(null, obj);
+})
+
+app.get('/login', passport.authenticate('auth0', {
+    sucessRedirect: '/students',
+    failureRedirect: '/login',
+    connection: 'github'
+    }
+));
 
 const port = 3000;
 app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
